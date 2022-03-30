b1696c9548d04dcea4b3f0ac743d2ea423a422a1
MasliaievKirill <kirillmaslyaev@gmail.com>
2022-03-30 19:22:21 +0300

    MainActivity works.

---
+++ app/src/main/java/com/masliaiev/filmspace/presentation/activities/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.masliaiev.filmspace.presentation.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.masliaiev.filmspace.R
+import com.masliaiev.filmspace.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.mainToolbar.setLogo(R.drawable.astronaut_logo)
+
+
+        setSupportActionBar(binding.mainToolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
